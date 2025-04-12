8aafd20e058cc44fbc34b93c6ee6cb107d930c47
hieuduong201 <hieuduong2012004@gmail.com>
2025-04-12 12:58:13 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import './App.css';
 
 import React from "react";
-import { Grid, Typography, Paper } from "@mui/material";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Grid, Paper } from "@mui/material";
+import { BrowserRouter as Router, Route, Routes, useMatch } from "react-router-dom";
 
 import TopBar from "./components/TopBar";
 import UserDetail from "./components/UserDetail";
 import UserList from "./components/UserList";
 import UserPhotos from "./components/UserPhotos";
+import models from "./modelData/models";
+
+const Layout = ({ children }) => {
+  const matchUsers = useMatch("/users/:userId");
+  const matchPhotos = useMatch("/photos/:userId");
+  
+  let currentUserName = "";
+  if (matchUsers && matchUsers.params.userId) {
+    const user = models.userModel(matchUsers.params.userId);
+    currentUserName = user ? `${user.first_name} ${user.last_name}` : "";
+  } else if (matchPhotos && matchPhotos.params.userId) {
+    const user = models.userModel(matchPhotos.params.userId);
+    currentUserName = user ? `${user.first_name} ${user.last_name}` : "";
+  }
+
+  return (
+    <>
+      <TopBar currentUserName={currentUserName} />
+      {children}
+    </>
+  );
+};
 
 const App = (props) => {
   return (
       <Router>
-        <div>
+        <Layout>
           <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TopBar />
-            </Grid>
             <div className="main-topbar-buffer" />
             <Grid item sm={3}>
               <Paper className="main-grid-item">
@@ -26,22 +45,16 @@ const App = (props) => {
             <Grid item sm={9}>
               <Paper className="main-grid-item">
                 <Routes>
-                  <Route
-                      path="/users/:userId"
-                      element = {<UserDetail />}
-                  />
-                  <Route
-                      path="/photos/:userId"
-                      element = {<UserPhotos />}
-                  />
+                  <Route path="/users/:userId" element={<UserDetail />} />
+                  <Route path="/photos/:userId" element={<UserPhotos />} />
                   <Route path="/users" element={<UserList />} />
                 </Routes>
               </Paper>
             </Grid>
           </Grid>
-        </div>
+        </Layout>
       </Router>
   );
-}
+};
 
 export default App;
