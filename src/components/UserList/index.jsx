9f63b096788b9d181b425575a122fd996bd00d69
hieuduong201 <hieuduong2012004@gmail.com>
2025-05-18 14:19:05 +0700

    finish front end

--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { List, ListItem, ListItemText } from '@mui/material';
 import models from '../../modelData/models';
+import { useEffect,useState } from 'react';
+import fetchModel from '../../lib/fetchModelData';
 
 const UserList = () => {
-  const users = models.userListModel();
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetchModel("/api/user")
+      .then((data) => setUsers(data))
+      .catch((err) => console.error(err));
+  }, []);
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/user`);
+  //     const data = await response.json();
+  //     setUsers(data);
+  //   };
+  //   fetchUsers();
+
+  // },[])
 
   return (
     <List>
@@ -15,7 +31,7 @@ const UserList = () => {
           component={Link}
           to={`/users/${user._id}`}
         >
-          <ListItemText primary={`${user.first_name} ${user.last_name}`} />
+          <ListItemText primary={` ${user.first_name} ${user.last_name}`} />
         </ListItem>
       ))}
     </List>
--- src/components/UserPhotos/index.jsx
+++ src/components/UserPhotos/index.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Card, CardContent, Typography } from '@mui/material';
 import models from '../../modelData/models';
-
+import { useEffect,useState } from 'react';
+import fetchModel from '../../lib/fetchModelData';
 const UserPhotos = () => {
   const { userId } = useParams();
-  const photos = models.photoOfUserModel(userId);
-
+  const [photos, setPhotos] = useState([]);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetchModel(`/api/photo/${userId}`)
+      .then(data => setPhotos(data))
+      .catch(err => console.error(err));
+    fetchModel("/api/user")
+      .then(data => setUsers(data))
+      .catch(err => console.error(err));
+  }, []);
   return (
     <div>
       {photos.map(photo => (
@@ -23,10 +32,17 @@ const UserPhotos = () => {
             {photo.comments && photo.comments.map(comment => (
               <div key={comment._id} style={{ marginTop: '8px' }}>
                 <Typography variant="caption">
-                  {new Date(comment.date_time).toLocaleString()} -{' '}
-                  <Link to={`/users/${comment.user._id}`}>
-                    {`${comment.user.first_name} ${comment.user.last_name}`}
-                  </Link>: {comment.comment}
+                  <p >{new Date(comment.date_time).toLocaleString()}</p>
+                  {users.find(user => user._id === comment.user_id) ? (
+                  <Link to={`/users/${comment.user_id}`}>
+                    {users.find(user => user._id === comment.user_id).first_name}{" "}
+                    {users.find(user => user._id === comment.user_id).last_name}
+                  </Link>
+                ) : (
+                  <span>Unknown User</span>
+                )}
+
+                  : {comment.comment}
                 </Typography>
               </div>
             ))}
